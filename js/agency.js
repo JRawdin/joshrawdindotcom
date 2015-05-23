4184caeffc060ca378fe3211d66c9eb87381d320
JRawdin <joshua.rawdin@gmail.com>
2015-05-23 14:33:41 -0400

    First commit

---
+++ js/agency.js
@@ -0,0 +1,52 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Skills bars
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 1000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+    $(this).animate({
+        height: 14,
+    }, 2000);
+	});
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
